cdcd471c226bc9d1ab219543e730e68264f18674
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-30 10:45:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,11 @@
 class Book
-attr_accessor :title, :author, :page_count, :genre
+attr_accessor :title, :author, :page_count, :genre, :page
 def initialize(title)
   @title = title
 end
 
-
-
-
-
-
-
-
+  def turn_page
+    page += 1
+  end
 
 end
